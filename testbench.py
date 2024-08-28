3ba5cc34f3cd7496670c6a95cf4ea9d0cc15edbb
Pascal-Emmanuel Lachance <17782420+Raesangur@users.noreply.github.com>
2024-08-28 16:52:54 -0400

    Add files via upload

---
+++ testbench.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta
+import cocotb
+
+
+class Testbench(metaclass=ABCMeta):
+    self.generator  = None
+    self.monitorIn  = None
+    self.monitorOut = None
+    self.checker    = None
+    self.model      = None
+    self.driver     = None
+
+
+    def __init__(self):
+        print("Starting test: " + self.__class__.__name__)
+        self._trace_debug()
+        
+
+    def reset_and_init(self):
+        self.driver.reset()
+        self.driver.init(self.generator)
+
+    @abc.abstractmethod
+    def run(self):
+        pass
+
+    
+    def _trace_debug():
+        PYCHARMDEBUG = os.environ.get('PYCHARMDEBUG')
+        print(PYCHARMDEBUG)
+
+        if(PYCHARMDEBUG == "enabled"):
+            pydevd_pycharm.settrace('localhost', port=5333, stdoutToServer=True, stderrToServer=True)
